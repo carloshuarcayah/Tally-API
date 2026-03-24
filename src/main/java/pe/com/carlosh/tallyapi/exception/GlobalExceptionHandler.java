6b05765ddf3c6d8bd583e16622f956c9e1ed9522
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-24 18:54:23 -0500

    feat(exception): add ExceptionHandler

---
+++ src/main/java/pe/com/carlosh/tallyapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package pe.com.carlosh.tallyapi.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest req){
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                req.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
