aac9ebd8829b61a2585f7ff9621662ec289a62df
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-24 20:11:42 -0500

    feat(category): add repository

--- src/main/java/pe/com/carlosh/tallyapi/category/CategoryRepository.java
+++ src/main/java/pe/com/carlosh/tallyapi/category/CategoryRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findById(Long id);
     Page<Category> findByActiveTrue(Pageable pageable);
     Page<Category> findByActiveFalse(Pageable pageable);
     Page<Category> findByNameContainingIgnoreCaseAndActiveTrue(String name, Pageable pageable);
+    Optional<Category> findByIdAndActiveTrue(Long id);
+
+    boolean existsByNameIgnoreCase(String name);
 }
